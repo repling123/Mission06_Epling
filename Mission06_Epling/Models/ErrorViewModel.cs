3d0acbeecc926fd6af6e83787b78503255601667
Rusell <russellepling@gmail.com>
2025-02-10 10:06:36 -0700

    Add project files.

---
+++ Mission06_Epling/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission06_Epling.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
